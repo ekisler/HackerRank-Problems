a4ad5fbdc48bc64fdfbc510a86b798fd32ccdbdc
ekisler <ekisler@gmail.com>
2022-04-14 17:28:58 -0400

    change15

---
+++ Maximum Subarray Sum.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'maximumSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. LONG_INTEGER_ARRAY a
+ *  2. LONG_INTEGER m
+ */
+
+long maximumSum(vector<long> a, long m) {
+    long sum = 0, max = LONG_MIN, result = LONG_MAX;
+    set<long> s;
+    
+    for (int i = 0; i < a.size(); i++) {
+        sum = (sum + a[i]) % m;
+        a[i] = sum;
+        max = std::max(max, sum);
+    }
+    
+    for (auto x: a) {
+        auto p = s.insert(x);
+        if (++p.first != s.end()) {
+            result = min(*p.first - x, result);
+        }
+    }
+    
+    return std::max(max, m - result);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        long m = stol(first_multiple_input[1]);
+
+        string a_temp_temp;
+        getline(cin, a_temp_temp);
+
+        vector<string> a_temp = split(rtrim(a_temp_temp));
+
+        vector<long> a(n);
+
+        for (int i = 0; i < n; i++) {
+            long a_item = stol(a_temp[i]);
+
+            a[i] = a_item;
+        }
+
+        long result = maximumSum(a, m);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
